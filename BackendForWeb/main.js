1b5160bd0f5a3ca09507489408a9f7e4a2b48c28
SECKIN BOSTANCI <seckinbostanci@gmail.com>
2018-04-19 20:35:51 +0300

    WebSocket with Dummy Auth

--- BackendForWeb/main.js
+++ BackendForWeb/main.js
@@ -1,25 +1,29 @@
-//websocket gateway on 8070
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var mysocket = 0;
-app.listen(8070);
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-io.sockets.on('connection', function (socket) {
-  console.log('index.html connected'); 
-  mysocket = socket;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+    console.log("ID'si : " + socket.id + " olan client ile socket bağlantısı başlatıldı..");
+    socket.on("auth",function(data){
+        if(data =='123'){
+            mysocket = socket;
+        }else{
+            socket.disconnect(true);
+        }
+    });
+    socket.on('disconnect', function(){
+        console.log("ID'si : " + socket.id + " olan client ile socket bağlantısı bitirildi..");
+    });
+  });
+
+http.listen(3000, function(){
+  console.log('Web Yayını Başlatıldı PORT:3000');
+});
 
 // net.socket ile backend'e bağlanıp dataları web socket'e attığımız kısım +
 var net = require('net');
@@ -31,12 +35,13 @@ client.connect(1007, '192.168.8.109', function () {
 });
 
 client.on('data', function (data) {
-    console.log(data);
-    
+    //console.log(data);
+    var msg = data;
+    msg = msg.slice(0, -1);
     //web sockete attığımız kısım.
     if (mysocket != 0) {
-        mysocket.emit('field', "" + data);
-        mysocket.broadcast.emit('field', "" + data);
+        mysocket.emit('data', "" + msg);
+        mysocket.broadcast.emit('data', "" + msg);
     }
     //client.destroy(); // kill client after server's response
 });
